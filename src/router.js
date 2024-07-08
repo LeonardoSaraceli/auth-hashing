9375d51c105802ea03c437f0b4f58f302b42f187
Leonardo Lodi <your_email@address>
2024-07-08 13:33:10 +0100

    add register and login router functions

--- src/router.js
+++ src/router.js
@@ -6,12 +6,50 @@ const prisma = new PrismaClient();
 
 const router = express.Router();
 
+const secret = process.env.JWT_SECRET_KEY
+
 router.post('/register', async (req, res) => {
-    
+    const { username, password } = req.body
+    const passwordHash = await bcrypt.hash(password, 8)
+    const user = await prisma.user.create({
+        data: {
+            username: username,
+            password: passwordHash
+        }
+    })
+
+    res.status(201).json({
+        user
+    })
 });
 
 router.post('/login', async (req, res) => {
-    
+    const { username, password } = req.body
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username
+        }
+    })
+
+    if (user) {
+        const match = await bcrypt.compare(password, user.password)
+
+        if (!match) {
+            res.status(401).json({
+                error: "The password provided doesn't match"
+            })
+        }
+
+        const token = jwt.sign({ "username": username }, secret)
+
+        res.status(201).json({
+            token
+        })
+    } else {
+        res.status(401).json({
+            error: "The username provided doesn't exist"
+        })
+    }
 });
 
 module.exports = router;
